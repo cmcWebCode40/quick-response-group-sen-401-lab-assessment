79c1bbd469631510c9d7608d9d210b5261b9ab8c
Michael Chinweike <infinity_michael@yahoo.com>
2026-03-29 22:40:23 +0100

    Add student data with names and scores

---
+++ task-1/students.py
@@ -0,0 +1,12 @@
+students = [
+    {"name": "Okechukwu Ogbonnaya", "score": 88},
+    {"name": "Kelly Ikenga", "score": 72},
+    {"name": "Michael Lawrence", "score": 95},
+    {"name": "Michael Chinonso Chinweike", "score": 64},
+    {"name": "Isaac Owonifaari", "score": 81},
+    {"name": "Chidera Omeje", "score": 90},
+    {"name": "Confidence Ogboarote Ojenomoh", "score": 55},
+    {"name": "Joshua Arijesuye", "score": 78},
+    {"name": "Alali Tobin", "score": 92},
+    {"name": "Onyedikachi John Eluwa", "score": 67},
+]
